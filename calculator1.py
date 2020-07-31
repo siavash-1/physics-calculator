b25bebfa3f74465a65ddef1ad6e076a04052446a
siavash <69014041+siavash-1@users.noreply.github.com>
2020-07-31 10:52:29 +0200

    Update calculator1.py

--- calculator1.py
+++ calculator1.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'heightcalc5.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
